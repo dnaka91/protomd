d3302d0b034a4b99a4867266c3abdc53a79da369
Dominik Nakamura <dnaka91@gmail.com>
2025-04-18 14:25:04 +0200

    feat: make method type configurable

--- src/config.rs
+++ src/config.rs
@@ -17,6 +17,12 @@ pub struct Conf {
     /// Configuration for `markdownlint`.
     #[config(nested)]
     pub markdownlint: Markdownlint,
+    /// Symbols used to define the gRPC method type in templates.
+    ///
+    /// Note that these are always encapsulated in backticks to better highlight them in the
+    /// resulting Markdown, therefore HTML.
+    #[config(nested)]
+    pub request_symbols: RequestSymbols,
 }
 
 #[derive(Clone, confique::Config, JsonSchema, Serialize)]
@@ -24,6 +30,22 @@ pub struct Markdownlint {
     /// List of rules to disable.
     #[config(default = [])]
     pub disable: Vec<String>,
+}
+
+/// Symbols, as in any kind of textual identifier, to describe the different kinds of gRPC calls.
+#[derive(Clone, confique::Config, JsonSchema, Serialize)]
+pub struct RequestSymbols {
+    #[config(default = "unary")]
+    pub unary: String,
+    /// Client-side streaming.
+    #[config(default = "client streaming")]
+    pub client_streaming: String,
+    /// Server-side streaming.
+    #[config(default = "server streaming")]
+    pub server_streaming: String,
+    /// Bidirectional (both client- and server-side) streaming.
+    #[config(default = "bidirectional streaming")]
+    pub bidi_streaming: String,
 }
 
 pub fn load() -> Result<Conf> {
